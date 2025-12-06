9e4a2bb69fca3400a37667c1532a0e1321545a5a
Tamer Vassib <tvassib@gmail.com>
2025-12-06 23:09:17 +0000

    Main functionaility

---
+++ IsoCubeTimerUITests/IsoCubeTimerApp.swift
@@ -0,0 +1,21 @@
+//
+//  IsoCubeTimerApp.swift
+//  IsoCubeTimer
+//
+//  Created by Tamer Vassib on 06/12/2025.
+//
+
+import SwiftUI
+import Foundation
+import GameplayKit
+
+
+@main
+struct IsoCubeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
